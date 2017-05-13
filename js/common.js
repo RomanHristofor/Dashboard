9c350a80c6f7c2ced18c30103888df4edb61d2af
Roman Khrystoforov <hristophorov.roman@gmail.com>
2017-05-13 11:00:27 +0300

    First pages commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	// Custom JS
+
+	$('#circle').click(function(){
+		$(this).toggleClass('active');
+	});
+
+	$('.icon-ctrl').click(function(){
+		$(this).toggleClass('active');
+	});
+
+});
